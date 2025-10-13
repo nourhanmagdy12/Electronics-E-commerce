d40ef56d3f6de9225f15d31e22d316834a32fff7
Nourhan Magdy <nourhanmagdy201@gmail.com>
2025-10-14 01:34:56 +0300

    Remainig files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,12 @@
 import { Routes } from '@angular/router';
-import { home } from './components/home/home';
-import { About } from './components/about/about';
-import { Contact } from './components/contact/contact';
-import { Products } from './components/products/products';
-import { NotFound } from './components/not-found/not-found';
 import { Cart } from './components/cart/cart';
 import { Checkout } from './components/checkout/checkout';
 
 export const routes: Routes = [
-  {path:'', redirectTo:'home', pathMatch:'full'},
-  {path:'home', component:home, title:'Home'},
-  {path:'about', component:About, title:"About"},
-  {path:'contact',component:Contact, title:'Contact'},
-  {path:'products',component:Products, title:'Products'},
+
+  
   {path:'cart',component:Cart, title:'Cart',},
   {path:'Checkout',component:Checkout, title:'Checkout'},
-  {path:'**',component:NotFound, title:'NotFound'}
+
 
 ];
