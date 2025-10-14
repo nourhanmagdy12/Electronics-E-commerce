e2b12d755a8b8b36f38993cdf9ae40fc74111a98
Nourhan Magdy <nourhanmagdy201@gmail.com>
2025-10-15 02:28:13 +0300

    Updated_Design

--- src/app/Components/footer/footer.ts
+++ src/app/Components/footer/footer.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
 
 
-@Component({ selector: 'app-footer', templateUrl: './footer.html' })
+@Component({ selector: 'app-footer', templateUrl: './footer.html' , styleUrls: ['./footer.css'], standalone: true })
 export class FooterComponent {}
